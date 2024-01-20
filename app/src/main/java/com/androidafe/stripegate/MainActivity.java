61d6e779d8a289359097ca41f314d654533f662d
0xRahad <haxorrahad@gmail.com>
2024-01-20 16:13:44 +0600

    Add feature of double value payment

--- app/src/main/java/com/androidafe/stripegate/MainActivity.java
+++ app/src/main/java/com/androidafe/stripegate/MainActivity.java
@@ -19,18 +19,18 @@ package com.androidafe.stripegate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 
     }
